2e31c9448b4bc44a8a03c95d2424b171fa1752e9
Pedro <pedro.phsr2002@gmail.com>
2024-01-22 21:07:20 -0300

    primeiro projeto

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System. Globalization;
+
+class program{
+    static void Main (string [] args)
+    {   
+        double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double delta = Math.Pow(b, 2) - 4 * a * c;
+
+        if (delta < 0)
+        {
+        Console.WriteLine("Não há raízes reais para esta equação.");
+        }
+        else
+        {
+        double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+        double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+        Console.WriteLine($"A primeira raiz é {x1.ToString("F2", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"A segunda raiz é {x2.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
